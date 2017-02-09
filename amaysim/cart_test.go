7d014dbfa777423fc643479faeae7efe9c216df2
Niko Mejia <mejia.niko@gmail.com>
2017-02-09 12:04:10 +0800

    Added more tests on cart_test.go and comments in rules.go

--- amaysim/cart_test.go
+++ amaysim/cart_test.go
@@ -1,6 +1,7 @@
 package amaysim_test
 
 import (
+	"reflect"
 	"testing"
 
 	. "github.com/nmeji/shoppingcart/amaysim"
@@ -40,16 +41,16 @@ func TestAdd(t *testing.T) {
 }
 
 func TestRemove(t *testing.T) {
-	shoppincart := ShoppingCart{Added: map[string]*CartItem{
+	cart := ShoppingCart{Added: map[string]*CartItem{
 		"ult_large": CreateCartItem(5, 0.0, "ult_large", "Unlimited 5GB", 44.90),
 	},
 	}
 	itemToRemove := CreateCartItem(2, 0.0, "ult_large", "Unlimited 5GB", 44.90)
 
 	// calling cart.remove(item) that does not reduce the quantity to 0 should still exist in the cart
-	shoppincart.Remove(*itemToRemove)
+	cart.Remove(*itemToRemove)
 	expectedQuantityLeft := 3
-	if item, ok := shoppincart.ItemsAdded()["ult_large"]; !ok {
+	if item, ok := cart.ItemsAdded()["ult_large"]; !ok {
 		t.Error("Assert failed: ShoppingCart.Remove(CartItem) - Item is removed from the cart even though quantity > 0")
 	} else if item.Quantity != expectedQuantityLeft {
 		t.Error("Assert failed: ShoppingCart.Remove(CartItem) - Item quantity did not match with expected quantity")
@@ -57,12 +58,114 @@ func TestRemove(t *testing.T) {
 
 	// callinig cart.remove(item) that reduces item quantity to zero or less should also remove the item from the cart
 	itemToRemove.Quantity = 4 // after removing this much, item quantity = -1
-	shoppincart.Remove(*itemToRemove)
-	if _, ok := shoppincart.ItemsAdded()["ult_large"]; ok {
+	cart.Remove(*itemToRemove)
+	if _, ok := cart.ItemsAdded()["ult_large"]; ok {
 		t.Error("Assert failed: ShoppingCart.Remove(CartItem) - After quantity drops to zero or less, it should be removed from the cart")
 	}
 }
 
 func TestAddPromoCode(t *testing.T) {
+	// 1. test for unknown promo code (should have no effect)
+	// 2. test for known promo code (eg. I<3AMAYSIM)
+	cart := ShoppingCart{
+		PricingRules: AllRules, // contains rule for promo code "I<3AMAYSIM"
+		Added:        make(map[string]*CartItem),
+		PromoCode:    make(map[string]int),
+	}
+	cart.Add(*CreateCartItem(1, 0.0, "ult_small", "Unlimited 1GB", 24.90))
+	cart.Add(*CreateCartItem(1, 0.0, "1gb", "1 GB Data-Pack", 9.90))
+	cart.AddPromoCode("QWERTY")
+	total := cart.Total()
+	expectedPrice := float32(34.80) // no discount
+	if total != expectedPrice {
+		t.Error("Assert failed: ShoppingCart.AddPromoCode(string) - Expected price did not match after unknown promo code is applied")
+	}
+	cart.AddPromoCode("I<3AMAYSIM")
+	total = cart.Total()
+	expectedDiscountedPrice := float32(31.32) // 10% discount
+	if total != expectedDiscountedPrice {
+		t.Error("Assert failed: ShoppingCart.AddPromoCode(string) - Expected discounted price did not match after promo code is applied")
+	}
+}
 
+func TestUsesPromoCode(t *testing.T) {
+	cart := ShoppingCart{PromoCode: map[string]int{
+		"I<3AMAYSIM": 1,
+	},
+	}
+	// 1. test for missing promo code
+	if cart.UsesPromoCode("QWERTY") {
+		t.Error("Assert failed: ShoppingCart.UsesPromoCode(string) - Expected false for promo codes not added")
+	}
+	// 2. test for added promo code
+	if !cart.UsesPromoCode("I<3AMAYSIM") {
+		t.Error("Assert failed: ShoppingCart.UsesPromoCode(string) - Expected true for promo codes already added")
+	}
+}
+
+func TestPromoCodesApplied(t *testing.T) {
+	cart := ShoppingCart{PromoCode: map[string]int{
+		"I<3AMAYSIM": 1,
+		"QWERTY":     1,
+	},
+	}
+	// should match with all added promo codes
+	if !reflect.DeepEqual([]string{"I<3AMAYSIM", "QWERTY"}, cart.PromoCodesApplied()) {
+		t.Error("Assert failed: ShoppingCart.PromoCodesApplied() - Expected match")
+	}
+}
+
+func MatchTotal(input []*CartItem, expected float32) bool {
+	cart := ShoppingCart{Added: make(map[string]*CartItem)}
+	for _, item := range input {
+		cart.Add(*item)
+	}
+	return expected == cart.Total()
+}
+
+func TestTotal(t *testing.T) {
+	// 1. should compute zero if no items are added yet
+	// 2. should have correct computation of summation of subtotals
+	tests := []struct {
+		Input    []*CartItem
+		Expected float32
+	}{
+		{nil, 0},
+		{[]*CartItem{
+			CreateCartItem(1, 0.0, "a", "A", 24.90),
+			CreateCartItem(1, 0.0, "b", "B", 9.90),
+		},
+			float32(34.80),
+		},
+	}
+	for _, test := range tests {
+		if !MatchTotal(test.Input, test.Expected) {
+			t.Error("Assert failed: ShoppingCart.Total() - Expected match")
+		}
+	}
+}
+
+func TestItems(t *testing.T) {
+	cart := ShoppingCart{Added: make(map[string]*CartItem)}
+	// should contain items added
+	input := CreateCartItem(1, 0, "a", "A", 1.0)
+	cart.Add(*input)
+	allItems := cart.Items()
+	found := false
+	for _, item := range allItems {
+		found = found || input.Equals(&item)
+	}
+	if !found {
+		t.Error("Assert failed: ShoppingCart.Items() - Expected item exists after being added")
+	}
+}
+
+func TestItemsAdded(t *testing.T) {
+	cart := ShoppingCart{Added: make(map[string]*CartItem)}
+	// should contain items added
+	input := CreateCartItem(1, 0, "a", "A", 1.0)
+	cart.Add(*input)
+	if item, ok := cart.ItemsAdded()["a"]; !ok || !input.Equals(item) {
+		t.Error("Assert failed: ShoppingCart.ItemsAdded() - Expected item exists after being added")
+	}
 }
--- amaysim/rules.go
+++ amaysim/rules.go
@@ -1,10 +1,20 @@
 package amaysim
 
+// Buy3For2 = For every 3 "Unlimited 1GB" sim card, pay only for 2
 type Buy3For2 struct{}
+
+// Buy1Get1 = For each "Unlimited 2GB" sim card you buy, you get 1 "1 GB Data-Pack"
 type Buy1Get1 struct{}
+
+// BulkDiscountOn5gb = If you buy more than 3 "Unlimited 5GB" sims, adjust the price to 39.90 for each
 type BulkDiscountOn5gb struct{}
+
+// OverallDiscountPromo = Enter "I<3AMAYSIM" as promo code and you'll get 10% discount on all items
 type OverallDiscountPromo struct{}
 
+/*
+Apply Buy3For2 rule
+*/
 func (rule Buy3For2) Apply(cart Cart) []CartItem {
 	added := cart.ItemsAdded()
 	if item, ok := added["ult_small"]; ok {
@@ -15,6 +25,9 @@ func (rule Buy3For2) Apply(cart Cart) []CartItem {
 	return nil
 }
 
+/*
+Apply Buy1Get1 rule
+*/
 func (rule Buy1Get1) Apply(cart Cart) []CartItem {
 	added := cart.ItemsAdded()
 	if item, ok := added["ult_medium"]; ok {
@@ -29,6 +42,9 @@ func (rule Buy1Get1) Apply(cart Cart) []CartItem {
 	return nil
 }
 
+/*
+Apply BulkDiscountOn5gb rule
+*/
 func (rule BulkDiscountOn5gb) Apply(cart Cart) []CartItem {
 	added := cart.ItemsAdded()
 	if item, ok := added["ult_large"]; ok {
@@ -39,6 +55,9 @@ func (rule BulkDiscountOn5gb) Apply(cart Cart) []CartItem {
 	return nil
 }
 
+/*
+Apply OverallDiscountPromo rule
+*/
 func (rule OverallDiscountPromo) Apply(cart Cart) []CartItem {
 	if cart.UsesPromoCode("I<3AMAYSIM") {
 		added := cart.ItemsAdded()
@@ -49,6 +68,7 @@ func (rule OverallDiscountPromo) Apply(cart Cart) []CartItem {
 	return nil
 }
 
+// AllRules available
 var AllRules []Rule
 
 func init() {
